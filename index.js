98e66afb33de883b7ddabd3bc5805acdeacf20c4
Sukhrob <khojiakbarmurodo05v@gmail.com>
2022-10-31 01:24:46 +0500

    arr

---
+++ index.js
@@ -0,0 +1,7 @@
+let arr = ['my name is', 'years old' , 'i live in', ' Xojiakbar', [' nefor city', ['im 16 ']]]
+
+console.log(
+    arr[0] + arr[3],
+    arr[2] + arr[4][0],
+    arr[4][1][0] + arr[1]
+);
